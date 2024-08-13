5650048f1d4b1079b22bb7ec8b96f7c83420e857
MisterG <g.haegerbaeumer@gmx.de>
2024-08-13 17:25:49 +0200

    Changed ID Generation

--- FahrzeugUtils.cs
+++ FahrzeugUtils.cs
@@ -1,14 +1,47 @@
-﻿namespace Tiefgarage
+﻿using System.DirectoryServices.ActiveDirectory;
+
+namespace Tiefgarage
 {
     public static class FahrzeugUtils
     {
-        private static int letzteVergebeneId = 0;
+        private static readonly List<string> vergebenIDs = new();
         public static string GibFreieId()
         {
-            letzteVergebeneId++;
-            return (letzteVergebeneId - 1).ToString();
+            string id = GenerateRandomID(new Random());
+            vergebenIDs.Add(id);
+            return id;
+        }
+
+        private static string GenerateRandomID(Random rnd)
+        {
+            string[] placesPrefixes = new string[]
+            {
+                "AB", "AZE", "BAD", "BI", "CUX", "DD", "DN", "DUD", "ER", "FÜ", "GUB", "Y", "ZZ", "WHV"
+            };
+
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+
+            string id = placesPrefixes[rnd.Next(0, placesPrefixes.Length)] + " ";
+
+            for (int i = 0; i < rnd.Next(1, 3); i++)
+            {
+                id += alphabet[rnd.Next(0, alphabet.Length)];
+            }
+
+            id += " ";
+
+            for (int i = 0; i < rnd.Next(1, 5); i++)
+            {
+                id += rnd.Next(0, 10);
+            }
+
+
+            if(vergebenIDs.Contains(id)) return GenerateRandomID(rnd);
+            return id;
         }
     }
+
+    
 
     public enum FahrzeugTyp
     {
